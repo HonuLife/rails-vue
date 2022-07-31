78f13a78ca4930d36636ae6e8b893da9fb5fb2a0
Joe Krump <joe.krump@clio.com>
2022-07-30 23:12:16 -0400

    Use TurbolinksAdapter

--- app/frontend/helpers/mount-component-on-turbo-load.ts
+++ app/frontend/helpers/mount-component-on-turbo-load.ts
@@ -1,11 +1,12 @@
 import { createApp, type Component } from "vue";
+import TurbolinksAdapter from "vue-turbolinks";
 
 function mountComponentOnTurboLoad(
   component: Component,
   rootContainer = "#vue-app"
 ) {
   document.addEventListener("turbo:load", () => {
-    createApp(component).mount(rootContainer);
+    createApp(component).use(TurbolinksAdapter).mount(rootContainer);
   });
 }
 
